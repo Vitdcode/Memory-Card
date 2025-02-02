c7989e35a991ac347ed5241de56b8adc46fd570e
Vitdcode <vitalijdietrich@gmail.com>
2025-02-02 19:16:47 +0100

    removed console log

--- src/game-logic/checkDuplicateIds.js
+++ src/game-logic/checkDuplicateIds.js
@@ -1,5 +1,4 @@
 export default function checkDuplicateIds(pokemonIdsRef, pokemonId) {
   const idIsDuplicate = pokemonIdsRef.current.some((id) => id === pokemonId);
-  console.log(idIsDuplicate);
   return idIsDuplicate;
 }
